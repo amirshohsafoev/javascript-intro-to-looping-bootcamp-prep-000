b3e7124631f5176af60ef4900d8816da4706f6c4
Amir Safoev <amirsafoev0@gmail.com>
2018-07-28 17:51:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,3 +19,12 @@ function whileLoop(number){
   }
   return "done";
 }
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
+}
